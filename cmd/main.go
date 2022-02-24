6011114e9c4572e4ec0939f0fd5b922f37d7635f
Illya Vernygora <illiavernigora@gmail.com>
2022-02-24 21:28:08 +0200

    Add read configuration from config file

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,26 @@ import (
 	"github.com/IlliaVern/go-todo-app/pkg/handler"
 	"github.com/IlliaVern/go-todo-app/pkg/repository"
 	"github.com/IlliaVern/go-todo-app/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("Error initializing configs: %s\n", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error ocurred while running http server: %s\n", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
