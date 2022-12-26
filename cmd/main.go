88faf3962f8583f3892887463243d8138a4e5c32
Illya Vernygora <illiavernigora@gmail.com>
2022-12-26 21:06:34 +0200

    Add graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/IlliaVern/go-todo-app"
 	"github.com/IlliaVern/go-todo-app/pkg/handler"
@@ -41,8 +44,25 @@ func main() {
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
-		logrus.Fatalf("Error ocurred while running http server: %s\n", err.Error())
+	go func() {
+		if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+			logrus.Fatalf("Error ocurred while running http server: %s\n", err.Error())
+		}
+	}()
+
+	logrus.Print("TodoApp Started...")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<- quit
+
+	logrus.Print("TodoApp Shutting Down...")
+
+	if err := srv.Shutdown(context.Background()); err == nil {
+		logrus.Errorf("error ocurred on server shutting down: %s", err.Error())
+	}
+	if err := db.Close(); err != nil {
+		logrus.Errorf("error ocurred on closing db connection: %s", err.Error())
 	}
 }
 
