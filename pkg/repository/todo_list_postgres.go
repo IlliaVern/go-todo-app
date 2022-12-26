91ea174d2ed44c729915f920f92d1173a91ff5fd
Illya Vernygora <illiavernigora@gmail.com>
2022-12-26 16:51:48 +0200

    Add updateById list API and related code structure

--- pkg/repository/todo_list_postgres.go
+++ pkg/repository/todo_list_postgres.go
@@ -2,9 +2,11 @@ package repository
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/IlliaVern/go-todo-app"
 	"github.com/jmoiron/sqlx"
+	"github.com/sirupsen/logrus"
 )
 
 type TodoListPostgres struct {
@@ -44,7 +46,8 @@ func (r *TodoListPostgres) GetAll(userId int) ([]todo.TodoList, error) {
 
 	query := fmt.Sprintf(`SELECT tl.id, tl.title, tl.description FROM %s tl
 						  INNER JOIN %s ul ON tl.id = ul.list_id
-						  WHERE ul.user_id = $1`,todoListsTable, usersListsTable)
+						  WHERE ul.user_id = $1`,
+						  todoListsTable, usersListsTable)
 	err := r.db.Select(&lists, query, userId)
 
 	return lists, err
@@ -55,7 +58,8 @@ func (r *TodoListPostgres) GetById(userId, listId int) (todo.TodoList, error) {
 
 	query := fmt.Sprintf(`SELECT tl.id, tl.title, tl.description FROM %s tl
 						  INNER JOIN %s ul ON tl.id = ul.list_id
-						  WHERE ul.user_id = $1 AND ul.list_id = $2`, todoListsTable, usersListsTable)
+						  WHERE ul.user_id = $1 AND ul.list_id = $2`,
+						  todoListsTable, usersListsTable)
 	err := r.db.Get(&list, query, userId, listId)
 
 	return list, err
@@ -63,8 +67,40 @@ func (r *TodoListPostgres) GetById(userId, listId int) (todo.TodoList, error) {
 
 func (r *TodoListPostgres) Delete(userId, listId int) error {
 	query := fmt.Sprintf(`DELETE FROM %s tl USING %s ul
-						  WHERE tl.id = ul.list_id AND ul.user_id = $1 AND ul.list_id = $2`, todoListsTable, usersListsTable)
+						  WHERE tl.id = ul.list_id AND ul.user_id = $1 AND ul.list_id = $2`,
+						  todoListsTable, usersListsTable)
 	_, err := r.db.Exec(query)
 
+	return err
+}
+
+func (r *TodoListPostgres) Update(userId, listId int, input todo.UpdateListInput) error {
+	setValues := make([]string, 0)
+	args := make([]interface{}, 0)
+	argId := 1
+
+	if input.Title != nil {
+		setValues = append(setValues, fmt.Sprintf("title=$%d", argId))
+		args = append(args, *input.Title)
+		argId++
+	}
+
+	if input.Description != nil {
+		setValues = append(setValues, fmt.Sprintf("description=$%d", argId))
+		args = append(args, *input.Description)
+		argId++
+	}
+
+	setQuery := strings.Join(setValues, ", ")
+	
+	query := fmt.Sprintf(`UPDATE %s tl SET %s FROM %s ul
+						  WHERE tl.id = ul.list_id AND ul.list_id = $%d AND ul.user_id = $%d`,
+						  todoListsTable, setQuery, usersListsTable, argId, argId+1)
+	args = append(args, listId, userId)
+
+	logrus.Debugf("Updated query: %s", query)
+	logrus.Debugf("Args: %s", args)
+	
+	_, err := r.db.Exec(query, args...)
 	return err
 }
